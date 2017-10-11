e958e4eaaad05fb8a5de7265e50f744946371ac3
Gethyn Jones <gethyn1@gmail.com>
2017-10-11 18:06:52 +0100

    Add avatar to producer card

--- src/components/ProducerCard.jsx
+++ src/components/ProducerCard.jsx
@@ -3,7 +3,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { ASSET_BASE } from '../config'
+
+import Avatar from './Avatar'
 import Distance from './Distance'
+import Icon from './Icon'
+
+/* eslint-disable no-unused-vars */
+import locationIcon from '../images/icons/location.svg'
+/* eslint-enable no-unused-vars */
 
 import styles from '../styles/6-components/_components.producer-card.scss'
 
@@ -25,9 +33,11 @@ const ProducerCard = ({ producer, lat, lng }: Props) => {
 
   return (
     <Link className={styles.card} to={`/producer/${producer.user_id}`}>
+      <Avatar size="small" className="u-margin-bottom" alt={producer.title} src={`${ASSET_BASE}/${producer.avatar}`} />
       <h2 className={styles.title}>{producer.title}</h2>
-      <p>{categories}</p>
+      <p className={styles.categories}>{categories}</p>
       <div className={styles.distance}>
+        <Icon type="location" size="12" />
         <Distance
           from={{ lat, lng }}
           to={{ lat: coords[1], lng: coords[0] }}
